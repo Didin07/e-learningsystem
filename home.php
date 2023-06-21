216e31738b0de1e042963532087cd5f4e6405bd5
silvia nurrobianti <silvianurrobianti@gmail.com>
2023-06-21 14:19:34 +0700

    project e-learningsystem bmkg ver 1

--- home.php
+++ home.php
@@ -1,31 +1,33 @@
 <!-- <h1><?php echo $title;?></h1> -->
 
 <div class="col-lg-12">  
-<h4>Mandate</h4>
-    <p> The Philippine Science High School System is an attached agency of the Department of Science and Technology (DOST).  The Board of Trustees (BOT), with the DOST Secretary as the Chairman, is the highest policy making body of the PSHS System. </p> 
+<h3>PROFIL</h3>
+    <p> Sejarah pengamatan meteorologi dan geofisika di Indonesia dimulai pada tahun 1841 diawali dengan pengamatan yang dilakukan secara perorangan oleh Dr. Onnen, Kepala Rumah Sakit di Bogor. Tahun demi tahun kegiatannya berkembang sesuai dengan semakin diperlukannya data hasil pengamatan cuaca dan geofisika.
+    Pada tahun 1866, kegiatan pengamatan perorangan tersebut oleh Pemerintah Hindia Belanda diresmikan menjadi instansi pemerintah dengan nama Magnetisch en Meteorologisch Observatorium atau Observatorium Magnetik dan Meteorologi dipimpin oleh Dr. Bergsma.
+    Pada tahun 1879 dibangun jaringan penakar hujan sebanyak 74 stasiun engamatan di Jawa. Pada tahun 1902 pengamatan medan magnet bumi dipindahkan dari Jakarta ke Bogor. Pengamatan gempa bumi dimulai pada tahun 1908 dengan pemasangan komponen horisontal seismograf Wiechert di Jakarta, sedangkan pemasangan komponen vertikal dilaksanakan pada tahun 1928.
+
+    Pada tahun 1912 dilakukan reorganisasi pengamatan meteorologi dengan menambah jaringan sekunder. Sedangkan jasa meteorologi mulai digunakan untuk penerangan pada tahun 1930.
+
+    Pada masa pendudukan Jepang antara tahun 1942 sampai dengan 1945, nama instansi meteorologi dan geofisika diganti menjadi Kisho Kauso Kusho.</p> 
 </div>
 <div  class="col-lg-6">
-  <h4>Vision</h4> 
-  <p>We are the leading science high school in the Asia Pacific Region preparing our scholars to become globally competitive Filipino scientists equipped with 21st century skills and imbued with the core values of truth, excellence, and service to nation.</p>
-  <h4>Mission</h4>
-  <p> The Philippine Science High School, operating under one System of Governance and Management, provides scholarship to students with high aptitude in science and mathematics.</p>
-  <h4>Core Values</h4> 
-  <ol>
-    <li>Pursuit of TRUTH.</li>  
-    <li>Passion for EXCELLENCE</li> 
-    <li>Commitment to SERVICE</li> 
+  <h3> VISI DAN MISI </h3>
+  <h4>VISI</h4> 
+  <p> Mewujudkan BMKG yang handal, tanggap dan mampu dalam rangka mendukung keselamatan masyarakat serta keberhasilan pembangunan nasional, dan berperan aktif di tingkat Internasional. 
+  Terminologi di dalam visi tersebut dapat dijelaskan sebagai berikut :
+  </p>
+  <ol type = "a">
+  <li> Pelayanan informasi meteorologi, klimatologi, kualitas udara, dan geofisika yang handal ialah pelayanan BMKG terhadap penyajian data, informasi pelayanan jasa meteorologi, klimatologi, kualitas udara, dan geofisika yang akurat, tepat sasaran, tepat guna, cepat, lengkap, dan dapat dipertanggungjawabkan </li>
+  <li>Tanggap dan mampu dimaksudkan BMKG dapat menangkap dan merumuskan kebutuhan stakeholder akan data, informasi, dan jasa meteorologi, klimatologi, kualitas udara, dan geofisika serta mampu memberikan pelayanan sesuai dengan kebutuhan pengguna jasa</li>
+</ol>
+  <h4>MISI</h4>
+  <p> Dalam rangka mewujudkan Visi BMKG, maka diperlukan visi yang jelas yaitu berupa langkah-langkah BMKG untuk mewujudkan Misi yang telah ditetapkan yaitu :</p>
+  <ol type = "a">
+      <li>Mengamati dan memahami fenomena meteorologi, klimatologi, kualitas udara dan geofisika.
+      </li>
+      <li>Menyediakan data, informasi dan jasa meteorologi, klimatologi, kualitas udara dan geofisika yang handal dan terpercaya.</li>
+      <li>Menyediakan data, informasi dan jasa meteorologi, klimatologi, kualitas udara dan geofisika yang handal dan terpercaya.</li>
+      <li>Berpartisipasi aktif dalam kegiatan internasional di Bidang meteorologi, klimatologi , kualitas udara dan geofisika</li>
   </ol>
-
-
-</div>
-<div  class="col-lg-6">
-  <h4>Guiding Principles</h4> 
-    <ul>
-      <li>Academic Freedom </li>
-      <li>Responsibility</li>
-      <li>Academic Standards</li>  
-    </ul>
-
-</div>
- 
+</div> 
 
